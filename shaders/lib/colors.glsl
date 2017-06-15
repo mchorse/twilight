6bf256222972897134a003934ddc093b7ca089c0
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-06-15 20:30:30 +0100

    Simplify colors (thanks to Jodie)

--- shaders/lib/colors.glsl
+++ shaders/lib/colors.glsl
@@ -1,5 +1,5 @@
 /* Colors */
 
-const vec3 zenith_color = vec3(190.0/255.0, 225.0/255.0, 238.0/255.0);
-const vec3 horizon_color = vec3(21/255.0, 103/255.0, 144.0/255.0);
-const vec3 night_color = vec3(50.0/255.0, 67.0/255.0, 145.0/255.0);
+const vec3 zenith_color = vec3(190, 225, 238) / 255.0;
+const vec3 horizon_color = vec3(21, 103, 144) / 255.0;
+const vec3 night_color = vec3(50, 67.0, 145) / 255.0;
